c96e8a6fb5ad884ac2509fe2f8de1c27c5951d1b
Christine Yoo <christinecho.s@gmail.com>
2021-02-26 15:09:01 -0800

    implemented dynamic rendering of Folder name

--- src/Folders/FolderOne.js
+++ src/Folders/FolderOne.js
@@ -3,9 +3,10 @@ import React, { Component } from "react";
 class FolderOne extends Component {
   // This function should check which folder the user clicked on and display the notes for that accordingly.
   displayFolderNotes = () => {
-    // const { folders } = this.props.state;
     const { notes } = this.props.state;
-    const filteredNotes = notes.filter((note) => note.folderId === this.props.match.params.folderId);
+    const filteredNotes = notes.filter(
+      (note) => note.folderId === this.props.match.params.folderId
+    );
     const formattedFilteredNotes = filteredNotes.map((note, i) => {
       return (
         <div key={i} className="note-card">
@@ -21,10 +22,12 @@ class FolderOne extends Component {
 
   // This function should return the formatted name of the folder that the user clicked on.
   displayFolderName = () => {
-    const {folders} = this.props.state;
-    const currentFolderObject = folders.find(folder => folder.id === this.props.match.params.folderId);
-    console.log(currentFolderObject);
-  }
+    const { folders } = this.props.state;
+    const currentFolderName = folders.filter(
+      (folder) => folder.id === this.props.match.params.folderId
+    )[0].name;
+    return <h3>{currentFolderName} Folder</h3>;
+  };
   render() {
     return (
       <>
